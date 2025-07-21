217685840e2c3c68f35eb87f0cb2d2e4f3a8f825
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-07-21 00:42:04 -0400

    first commit

---
+++ src/commands/commands/item.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fs = require("fs/promises");
+
+module.exports = {
+    data: {
+        name: "item",
+        description: "Get a random item from Pressure",
+        "integration_types": [0, 1],
+        "contexts": [0, 1, 2]
+    },
+    async execute(interaction) {
+
+      const itemsJsonData = await fs.readFile("data/items.json", {encoding: "utf8"});
+      const itemsMap = JSON.parse(itemsJsonData);
+      const items = itemsMap["items"];
+      const item = items[Math.floor(Math.random() * items.length)];
+
+      const embed = new EmbedBuilder()
+      .setTitle(`${item.name}`)
+      .setDescription()
+      .setThumbnail(`https://cdn.sylvee.xyz/${item.path}.png`)
+
+      await interaction.reply({ embeds: [embed] });
+    },
+};
---
+++ src/events/ready.js
@@ -0,0 +1,16 @@
+const { ActivityType } = require("discord.js");
+
+module.exports = {
+  name: "ready",
+  once: true,
+  async execute(client) {
+    client.user.setPresence({
+      status: "online",
+      activities: [{
+        type: ActivityType.Custom,
+        name: "customstatus",
+        state: "wawa",
+      }],
+    });
+  },
+};
